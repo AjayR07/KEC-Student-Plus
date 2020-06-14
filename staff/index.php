4c2f84d57f2ed80c9e96b1459b4b9fe302d7d12b
AbinashS <s.abinash891@gmail.com>
2020-06-14 17:05:33 +0530

    staff index

--- staff/index.php
+++ staff/index.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> b4c0f4b3be16687b7aad2fcea3aa8eb5e3af8e24
 <?php
 session_start();
 if(!isset($_SESSION['user']))
@@ -22,51 +18,116 @@ include_once('../assets/notiflix.php');
   <title>Staff Index</title>
 </head>
 
+
+<script>
+
+$(document).ready(function(){
+
+  $('.ui.inverted.accordion')
+  .accordion();
+
+});
+
+
+</script>
+
+
 <body>
   <style>
     body{
       background-image: url('../backstaff.jpg');
     }
-  </style>
+    .ui.inverted.segment {
+    padding-left:80px;
+    padding-top:30px;
+    padding-bottom:20px;
+    }
+    .content.active{
+        margin-left:50px;
+
+    }
+    .ui.segment{
+      background: #565454;
+    }
+    .ui.inverted.segment
+    {
+      border-radius: 5px;
+    }
+
+    </style>
+  <br>
   <div class="ui raised very padded container segment">
     <center><h1 class="ui header">Hi <?php echo $staff; ?>!</h1></center>
-    <div class="ui form">
-    <div class="content">
-    <div class="ui two fields">
-      <!-- Card 1 -->
-      <div class="ui inverted card">
-        <div class="content">
-          <div class="header">Permission Appl.</div>
-          <div class="meta">PreOd, Non-Cert Od</div>
-          <div class="description">
-<<<<<<< HEAD
-          
-=======
-            
->>>>>>> b4c0f4b3be16687b7aad2fcea3aa8eb5e3af8e24
+   <br><br>
+  <div class="ui two column very relaxed stackable grid">
+    <!-- FIRST CARD 1ST SEG -->
+    <div class="column">
+      <div class="ui inverted segment">
+         <div class="ui green inverted statistic">
+           <div class="value">
+            30
+            </div>
+              <div class="label">
+             OD Permission
+           </div>
+          </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div class="ui blue inverted statistic">
+         <div class="value">
+           19
           </div>
+           <div class="label" >
+            Non Certificate OD
+         </div>
         </div>
       </div>
-      <!-- Card 2 -->
-      <div class="ui inverted card">
-        <div class="content">
-          <div class="header">Certificate Appl.</div>
-          <div class="meta">PostOd, Other Cert</div>
-          <div class="description">
-           
+    </div>
+    <!-- FIRST CARD 2ND SEG -->
+    <div class="middle aligned column">
+    <div class="ui inverted segment">
+         <div class="ui green inverted statistic">
+           <div class="value">
+            5
+            </div>
+              <div class="label">
+             OD Certificate
+           </div>
+          </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div class="ui blue inverted statistic">
+         <div class="value">
+           18
           </div>
+           <div class="label" >
+            Other Certificate
+         </div>
         </div>
       </div>
-      </div>
-    </div>
-    </div>
-    
+     </div>
+  </div><br>
   </div>
+    <!-- CARD 2 -->
+  <div class="ui raised very padded container segment">
+  <div class="ui inverted segment">
+  <div class="ui inverted accordion">
+    <div class="title">
+      <i class="dropdown icon"></i>
+      Abinash S
+    </div>
+    <div class="content">
+    <p>Paper Presentation-Big Data , PSG ,Koyampuththoor.</p>
+    </div>
+    <div class="title">
+      <i class="dropdown icon"></i>
+      Kelavan GJ
+    </div>
+    <div class="content">
+      <p>Paper Presentation-Big Data , PSG ,Koyampuththoor.</p>
+    </div>
+  </div>
+ </div>
+</div><br>
+
+
 </body>
 
-<<<<<<< HEAD
-</html>
-=======
 </html>
 
->>>>>>> b4c0f4b3be16687b7aad2fcea3aa8eb5e3af8e24
