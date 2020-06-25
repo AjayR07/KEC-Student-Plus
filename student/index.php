67faf48909d4ba9eaed5e73b6e5e5d0423de57a6
AbinashS <s.abinash891@gmail.com>
2020-06-25 10:00:30 +0530

    Essen Comit

--- student/index.php
+++ student/index.php
@@ -1,30 +1,67 @@
-
-<?php
-session_start();
-if(!isset($_SESSION['uname']))
-{
-    header("Location: ../studLog.php");
-}
-$register=$_SESSION['uname'];
-include_once('../db.php');
-include_once('studentnav.php');
-include_once('../assets/notiflix.php');
-
-?>
-<html lang="en">
-
+<!DOCTYPE html>
+<html lang="en" >
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Student Index</title>
+  <title>Profile</title>
+  <link rel="stylesheet" href="css/profile.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+    .preloader {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 9999;
+      background-image: url('./images/giphy3.svg');
+      background-repeat: no-repeat;
+      background-color: #FFF;
+      background-size: auto;
+      background-position: center;
+    
+    }
+  </style>
+
+
+
 </head>
+<aside class="profile-card">
+  
+  <header>
+    <a>
+      <img src="images/matthew.png" class="hoverZoomLink">
+    </a>
+    <h1>John Doe</h1>
+    
+  </header>
 
-<body>
-  <div class="ui raised very padded text container segment">
-    <h2 class="ui header">Hi </h2>
-    
-    
+  <div class="profile-bio">
+    <p>
+      <label class="prf">Phone:</label>
+      <a>7871502525</a><br>
+      <div class="row" style="height: 10px;"></div>
+      <label class="prf">Mail Id:</label>
+      <i class="fa fa-envelope" style="color: purple;" aria-hidden="true"></i><br>
+      <div class="row" style="height: 10px;"></div>
+      <label class="prf">Total Applied:</label>
+      <a>3</a><br>
+      <div class="row" style="height: 10px;"></div>
+      <label class="prf">Total Pending:</label>
+      <a>2</a><br>
+      <div class="row" style="height: 10px;"></div>
+      <label class="prf">Total Granted:</label>
+      <a>4</a><br>
+      <div class="row" style="height: 10px;"></div>
+      <label class="prf">Total Rejected:</label>
+      <a>1</a><br>
+      <div class="row" style="height: 10px;"></div>
+      <label class="prf">Certificates Registered:</label>
+      <a>5</a><br>
+    </p>
+
   </div>
-</body>
 
+</aside>  
+</body>
 </html>
