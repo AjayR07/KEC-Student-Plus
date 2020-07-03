bcf05acf09ff28d7ceac6097d9bec8c80d526f17
AbinashS <s.abinash891@gmail.com>
2020-07-03 23:28:37 +0530

    Test Commit

--- staff/NonCertAprv.php
+++ staff/NonCertAprv.php
@@ -12,14 +12,10 @@ include_once('../assets/notiflix.php');
 <html lang="en">
 
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <link rel="icon" type="image/png" href="../KEC.png">
-
     <title>Local OD Approval</title>
-
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital@1&display=swap" rel="stylesheet">
 
     <link href="css/main.css" rel="stylesheet" media="all">
